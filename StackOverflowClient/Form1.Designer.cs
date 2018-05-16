7ec8a4d1eed78dbc74fe79acef6d9358c9c36e15
David Betteridge <DavidaBetteridge@gmail.com>
2018-05-16 09:11:55 +0100

    WIP

---
+++ StackOverflowClient/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace StackOverflowClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtSearchTerm = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmdSearch = new System.Windows.Forms.Button();
+            this.panelResults = new System.Windows.Forms.Panel();
+            this.cmdNoOfResults = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::StackOverflowClient.Properties.Resources.Stack_Overflow_Logo;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(463, 175);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtSearchTerm
+            // 
+            this.txtSearchTerm.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSearchTerm.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearchTerm.Location = new System.Drawing.Point(17, 219);
+            this.txtSearchTerm.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.txtSearchTerm.Name = "txtSearchTerm";
+            this.txtSearchTerm.Size = new System.Drawing.Size(973, 43);
+            this.txtSearchTerm.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 182);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 30);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Search For";
+            // 
+            // cmdSearch
+            // 
+            this.cmdSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdSearch.Location = new System.Drawing.Point(999, 219);
+            this.cmdSearch.Name = "cmdSearch";
+            this.cmdSearch.Size = new System.Drawing.Size(140, 43);
+            this.cmdSearch.TabIndex = 3;
+            this.cmdSearch.Text = "Search";
+            this.cmdSearch.UseVisualStyleBackColor = true;
+            this.cmdSearch.Click += new System.EventHandler(this.cmdSearch_Click);
+            // 
+            // panelResults
+            // 
+            this.panelResults.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelResults.AutoScroll = true;
+            this.panelResults.Location = new System.Drawing.Point(17, 285);
+            this.panelResults.Name = "panelResults";
+            this.panelResults.Size = new System.Drawing.Size(1121, 499);
+            this.panelResults.TabIndex = 4;
+            // 
+            // cmdNoOfResults
+            // 
+            this.cmdNoOfResults.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdNoOfResults.BackColor = System.Drawing.Color.White;
+            this.cmdNoOfResults.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmdNoOfResults.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cmdNoOfResults.FormattingEnabled = true;
+            this.cmdNoOfResults.Items.AddRange(new object[] {
+            "10",
+            "25",
+            "50",
+            "100",
+            "All"});
+            this.cmdNoOfResults.Location = new System.Drawing.Point(1018, 24);
+            this.cmdNoOfResults.Name = "cmdNoOfResults";
+            this.cmdNoOfResults.Size = new System.Drawing.Size(121, 38);
+            this.cmdNoOfResults.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(785, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(227, 30);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Max. number of results";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1157, 796);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmdNoOfResults);
+            this.Controls.Add(this.panelResults);
+            this.Controls.Add(this.cmdSearch);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSearchTerm);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "Form1";
+            this.Text = "Stackover Client";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtSearchTerm;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button cmdSearch;
+        private System.Windows.Forms.Panel panelResults;
+        private System.Windows.Forms.ComboBox cmdNoOfResults;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ StackOverflowClient/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StackOverflowClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            cmdNoOfResults.Text = "10";
+        }
+
+        private async void cmdSearch_Click(object sender, EventArgs e)
+        {
+            var searchTerm = this.txtSearchTerm.Text.Trim();
+            if (string.IsNullOrWhiteSpace(searchTerm))
+            {
+                MessageBox.Show("Please enter a search term", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            var topCount = "";
+            var numberOfResults = cmdNoOfResults.Text;
+            if (numberOfResults != "All")
+                topCount = " top " + numberOfResults;
+
+            var sb = new SqlConnectionStringBuilder
+            {
+                InitialCatalog = "StackOverflow2010",
+                DataSource = ".",
+                IntegratedSecurity = true
+            };
+            var cs = sb.ToString();
+
+            using (var cn = new SqlConnection(cs))
+            {
+                await cn.OpenAsync();
+
+                using (var cmd = new SqlCommand())
+                {
+                    cmd.Connection = cn;
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandTimeout = 0;
+                    cmd.CommandText = $@"SELECT {topCount} Id, Body, IsNull(Title,'') as Title
+                                          FROM dbo.Posts
+                                         WHERE Body like @SearchTerm";
+
+                    cmd.Parameters.AddWithValue("@SearchTerm", "%" + searchTerm + "%");
+
+                    this.Text = "Stack Overflow Client - Searching...";
+                    var results = 0;
+                    var sw = new Stopwatch();
+                    sw.Start();
+                    using (var dr = await cmd.ExecuteReaderAsync())
+                    {
+                        this.panelResults.Controls.Clear();
+
+                        var top = 30;
+                        var shaded = false;
+                        while (await dr.ReadAsync())
+                        {
+                            var lbl = new Label
+                            {
+                                Location = new Point(16, top),
+                                Size = new Size(panelResults.Width - 32, 30),
+                                TabIndex = 0,
+                                Text = Summary((string)dr["Title"], (string)dr["Body"]),
+                                Anchor = AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top
+                            };
+                            if (shaded) lbl.BackColor = Color.LightGray;
+                            this.panelResults.Controls.Add(lbl);
+                            shaded = !shaded;
+                            top += 40;
+                            results++;
+                        }
+                    }
+
+                    sw.Stop();
+                    this.Text = $"Stack Overflow Client - {results}results found in {Math.Round(sw.Elapsed.TotalSeconds,2)}s";
+
+
+                }
+
+
+            }
+
+        }
+
+        private string Summary(string title, string body)
+        {
+            title = title.Replace(System.Environment.NewLine, "");
+            body = body.Replace(System.Environment.NewLine, "");
+
+            if (!string.IsNullOrWhiteSpace(title))
+                return title;
+            else
+            {
+                if (body.Length >= 100)
+                    return body.Substring(0, 100) + "...";
+                else
+                    return body;
+            }
+        }
+    }
+}
