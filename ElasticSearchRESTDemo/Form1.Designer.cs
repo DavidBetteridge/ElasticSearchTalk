34bbeba07bdaca3a265c3d895c2a8b77c6c37f0c
David Betteridge <DavidaBetteridge@gmail.com>
2018-05-18 23:58:35 +0100

    WIP

---
+++ ElasticSearchRESTDemo/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace ElasticSearchRESTDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cmdExecute = new System.Windows.Forms.Button();
+            this.txtCommand = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbMethod = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblTimeTaken = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1255, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.BackColor = System.Drawing.Color.LightBlue;
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTitle.Location = new System.Drawing.Point(0, 24);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(1255, 58);
+            this.lblTitle.TabIndex = 12;
+            this.lblTitle.Text = "Demo Title Goes Here";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.cmdExecute);
+            this.panel1.Controls.Add(this.txtCommand);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.cbMethod);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtURL);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 82);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(636, 650);
+            this.panel1.TabIndex = 13;
+            // 
+            // cmdExecute
+            // 
+            this.cmdExecute.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdExecute.Location = new System.Drawing.Point(457, 597);
+            this.cmdExecute.Name = "cmdExecute";
+            this.cmdExecute.Size = new System.Drawing.Size(161, 31);
+            this.cmdExecute.TabIndex = 10;
+            this.cmdExecute.Text = "Execute";
+            this.cmdExecute.UseVisualStyleBackColor = true;
+            this.cmdExecute.Click += new System.EventHandler(this.cmdExecute_Click);
+            // 
+            // txtCommand
+            // 
+            this.txtCommand.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtCommand.Location = new System.Drawing.Point(17, 136);
+            this.txtCommand.Multiline = true;
+            this.txtCommand.Name = "txtCommand";
+            this.txtCommand.Size = new System.Drawing.Size(601, 458);
+            this.txtCommand.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Command:";
+            // 
+            // cbMethod
+            // 
+            this.cbMethod.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbMethod.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbMethod.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cbMethod.FormattingEnabled = true;
+            this.cbMethod.Items.AddRange(new object[] {
+            "GET",
+            "POST",
+            "PUT",
+            "DELETE"});
+            this.cbMethod.Location = new System.Drawing.Point(511, 47);
+            this.cbMethod.Name = "cbMethod";
+            this.cbMethod.Size = new System.Drawing.Size(107, 33);
+            this.cbMethod.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(536, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Method:";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(17, 52);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(393, 33);
+            this.txtURL.TabIndex = 5;
+            this.txtURL.Text = "http://localhost:9200";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "URL:";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.lblTimeTaken);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.txtResult);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(636, 82);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(619, 650);
+            this.panel2.TabIndex = 14;
+            // 
+            // lblTimeTaken
+            // 
+            this.lblTimeTaken.AutoSize = true;
+            this.lblTimeTaken.Location = new System.Drawing.Point(119, 24);
+            this.lblTimeTaken.Name = "lblTimeTaken";
+            this.lblTimeTaken.Size = new System.Drawing.Size(30, 25);
+            this.lblTimeTaken.TabIndex = 15;
+            this.lblTimeTaken.Text = "0s";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 24);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 25);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Time Taken:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 25);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Result:";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResult.Location = new System.Drawing.Point(18, 136);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(585, 458);
+            this.txtResult.TabIndex = 12;
+            // 
+            // splitter1
+            // 
+            this.splitter1.BackColor = System.Drawing.Color.LightBlue;
+            this.splitter1.Location = new System.Drawing.Point(636, 82);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 650);
+            this.splitter1.TabIndex = 15;
+            this.splitter1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = global::ElasticSearchRESTDemo.Properties.Resources.elastic;
+            this.pictureBox1.Location = new System.Drawing.Point(339, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(264, 89);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1255, 732);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Elastic Search REST Demo";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button cmdExecute;
+        private System.Windows.Forms.TextBox txtCommand;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbMethod;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblTimeTaken;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ElasticSearchRESTDemo/Form1.cs
@@ -0,0 +1,157 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ElasticSearchRESTDemo
+{
+
+    public partial class Form1 : Form
+    {
+        private readonly HttpClient _client;
+        public Form1()
+        {
+            InitializeComponent();
+            SetupMenus();
+
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri("http://localhost:9200");
+            _client.DefaultRequestHeaders
+                   .Accept
+                   .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        private async void cmdExecute_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var sw = new Stopwatch();
+                sw.Start();
+
+                switch (this.cbMethod.Text)
+                {
+                    case "GET":
+                        await DoGet();
+                        break;
+
+                    case "PUT":
+                        await DoPut();
+                        break;
+
+                    default:
+                        break;
+                }
+
+                sw.Stop();
+                lblTimeTaken.Text = $"{Math.Round(sw.Elapsed.TotalSeconds, 2)}s";
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Unexpected Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private async Task DoGet()
+        {
+            var url = txtURL.Text;
+            var result = await _client.GetStringAsync(url);
+            txtResult.Text = PrettyPrintJSON(result);
+        }
+
+        private async Task DoPut()
+        {
+            var url = this.txtURL.Text;
+            var command = this.txtCommand.Text;
+
+            var content = new StringContent(command, Encoding.UTF8, "application/json");
+            var result = await _client.PutAsync(url, content);
+            var json = await result.Content.ReadAsStringAsync();
+
+            txtResult.Text = PrettyPrintJSON(json);
+        }
+
+
+
+        private void SetupMenus()
+        {
+            var systemDemos = AddDemoCategory("System Demos");
+            var defaultDefinition = new DemoDefinition() { Title = "Overview", Method = "GET", URL = "/" };
+            AddDemo(systemDemos, defaultDefinition);
+            AddDemo(systemDemos, new DemoDefinition() { Title = "Cluster Health", Method = "GET", URL = "/_cluster/health" });
+
+            var gettingStarted = AddDemoCategory("Getting Started");
+            AddDemo(gettingStarted, new DemoDefinition() { Title = "Add Document", Method = "PUT", URL = "/meetups/dotnetyork/june2018", Content = @"{
+                                                                                            ""title"": ""Double Bill - Elasticsearch and JavaScript Performance"",
+                                                                                            ""speakers"":  ""David Betteridge and Benjamin Howarth"",
+                                                                                            ""date"":  ""Friday, June 1, 2018""
+                                                                                            }" });
+
+
+            DisplayDefinition(defaultDefinition);
+        }
+
+        private ToolStripMenuItem AddDemoCategory(string text)
+        {
+            var parentMenuItem = new ToolStripMenuItem();
+            parentMenuItem.Size = new Size(97, 20);
+            parentMenuItem.Text = text;
+            this.menuStrip1.Items.Add(parentMenuItem);
+            return parentMenuItem;
+        }
+
+        private void AddDemo(ToolStripMenuItem parentMenuItem, DemoDefinition demoDefinition)
+        {
+            var menuItem = new ToolStripMenuItem();
+            menuItem.Size = new Size(152, 22);
+            menuItem.Text = demoDefinition.Title;
+            menuItem.Click += MenuItem_Click;
+            menuItem.Tag = demoDefinition;
+            parentMenuItem.DropDownItems.Add(menuItem);
+        }
+
+        private void MenuItem_Click(object sender, EventArgs e)
+        {
+            if (sender is ToolStripMenuItem menu)
+            {
+                if (menu.Tag is DemoDefinition demo)
+                {
+                    DisplayDefinition(demo);
+                }
+            }
+        }
+
+        private void DisplayDefinition(DemoDefinition demo)
+        {
+            this.txtURL.Text = demo.URL;
+            this.cbMethod.Text = demo.Method;
+            this.txtCommand.Text = PrettyPrintJSON(demo.Content);
+            this.txtResult.Text = "";
+            this.lblTitle.Text = demo.Title;
+        }
+
+        private static string PrettyPrintJSON(string result)
+        {
+            if (string.IsNullOrWhiteSpace(result))
+                return "";
+            else
+                return JToken.Parse(result).ToString(Formatting.Indented);
+        }
+
+        private class DemoDefinition
+        {
+            public string Method { get; set; }
+            public string URL { get; set; }
+            public string Content { get; set; }
+            public string Title { get; set; }
+        }
+
+    
+    }
+}
